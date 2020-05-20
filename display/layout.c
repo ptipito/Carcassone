db40c563346c86cdd00e2147fbf192790ce7c2df
Pierre Nagellen <nagellen.pierre@gmail.com>
2020-05-20 08:55:04 +0200

    Add NULL checks on get_view results

--- display/layout.c
+++ display/layout.c
@@ -204,7 +204,7 @@ SDL_Surface* get_view(View_Type type, char* filename){
             break;
     }
     strncat(path_of_tile_view,filename,IMAGE_PATH_MAX_LEN-folder_len);
-    printf("%s\n",path_of_tile_view);
+
     return IMG_Load(path_of_tile_view);
 }
 
@@ -212,7 +212,6 @@ void details_show_tile(Layout* layout, SDL_Surface* tile){
     SDL_Rect pos = get_centering_pos(layout->details_surface,tile);
     pos.x = details_get_centering_absciss_pos(layout,tile);
     blit_on(LP_DETAILS,tile,NULL,&pos,layout);
-    printf("show turn controls: ");
     details_show_tile_turn_controls(layout,pos);
 }
 
@@ -221,20 +220,26 @@ void details_show_tile_turn_controls(Layout* layout, SDL_Rect tile_pos){
     int tile_size = get_tile_size_in_pixels(layout->tile_size),
         padding_with_tile = 5;
 
-    SDL_Surface* control = get_view(VT_CONTROL,"turn_left2.jpg");
-    pos.x = tile_pos.x - control->w - padding_with_tile;
-    pos.y += int_center(tile_size,control->h);
-    blit_on(LP_DETAILS,control,NULL,&pos,layout);
+    SDL_Surface* control = get_view(VT_CONTROL,"turn_left.jpg");
+    if(control!=NULL){
+        pos.x = tile_pos.x - control->w - padding_with_tile;
+        pos.y += int_center(tile_size,control->h);
+        blit_on(LP_DETAILS,control,NULL,&pos,layout);
+    }
 
-    control = get_view(VT_CONTROL,"turn_right.jpg");
-    pos.x = tile_pos.x + tile_size + padding_with_tile;
-    pos.y = tile_pos.y + int_center(tile_size,control->h);
-    blit_on(LP_DETAILS,control,NULL,&pos,layout);
+    if(control!=NULL){
+        control = get_view(VT_CONTROL,"turn_right.jpg");
+        pos.x = tile_pos.x + tile_size + padding_with_tile;
+        pos.y = tile_pos.y + int_center(tile_size,control->h);
+        blit_on(LP_DETAILS,control,NULL,&pos,layout);
+    }
 
-    control = get_view(VT_CONTROL,"turn_1802.jpg");
-    pos.x = tile_pos.x + int_center(tile_size,control->w);
-    pos.y = tile_pos.y + tile_size + padding_with_tile;
-    blit_on(LP_DETAILS,control,NULL,&pos,layout);
+    control = get_view(VT_CONTROL,"turn_180.jpg");
+    if(control!=NULL){
+        pos.x = tile_pos.x + int_center(tile_size,control->w);
+        pos.y = tile_pos.y + tile_size + padding_with_tile;
+        blit_on(LP_DETAILS,control,NULL,&pos,layout);
+    }
     //Safe to free here already, as the turn controls are used nowhere else and shouldn't be erased later
     SDL_FreeSurface(control);
 }
