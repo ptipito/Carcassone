5f853c9a3f4041aecf7ba5d56215d7251524be30
Pierre Nagellen <nagellen.pierre@gmail.com>
2020-05-20 09:17:40 +0200

    Refactor main

--- main.c
+++ main.c
@@ -7,7 +7,10 @@
 #include "model/board/carcassone_playboard.h"
 #include "model/board/rim.h"
 #include "tests/model/test_playboard.h"
-
+/*
+This file is not yet the real main and serves as testing for SDL display.
+For the time being is there no link between the model files and the functionalities available through the main
+*/
 void Quit(SDL_Window* window){
     if(window!=NULL){
         SDL_DestroyWindow(window);
@@ -23,24 +26,21 @@ int main(int argc, char* argv[]){
     }
     Quit(NULL);
 
-    SDL_Window* window = SDL_CreateWindow("Carcassone",0,30,1000,500,SDL_WINDOW_OPENGL);
+    SDL_Window* window = initialize();
     SDL_Event event;
     int done=0;
-    Layout layout=new_layout();
-    Layout* window_layout = &layout;
+    Layout* window_layout = new_layout();
     initialize_game_layout(window,window_layout);
 
     display_grid(window_layout);
 
-    SDL_Surface *cloister = NULL;
-    cloister = IMG_Load("ressources/images/cloister.jpg");
+    SDL_Surface *cloister = get_view(VT_TILE,"cloister.jpg");
     if(cloister==NULL)
         printf("cloister not loaded\n");
     SDL_Rect cloister_pos; cloister_pos.x=get_tile_size_in_pixels(MEDIUM_TILE);cloister_pos.y=get_tile_size_in_pixels(MEDIUM_TILE);
 
-    SDL_BlitSurface(cloister,NULL,window_layout->map_surface,&cloister_pos);
-    SDL_Surface *screen = SDL_GetWindowSurface(window_layout->window);
-    SDL_BlitSurface(window_layout->map_surface,NULL,screen,&(window_layout->map_pos));
+    map_insert_tile(cloister,cloister_pos.x,cloister_pos.y,window_layout);
+    details_show_tile(window_layout,cloister);
     SDL_UpdateWindowSurface(window_layout->window);
     while(!done){
         SDL_WaitEvent(&event);
@@ -59,7 +59,6 @@ int main(int argc, char* argv[]){
     }
 
     SDL_FreeSurface(cloister);
-    SDL_FreeSurface(screen);
     free_layout(window_layout);
     Quit(NULL);
 
