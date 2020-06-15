1d3f3a4b517199151c6efe5735098705d62ad447
Pierre Nagellen <nagellen.pierre@gmail.com>
2020-06-15 08:47:32 +0200

    [Feat] add utility function for NULL pointer testing with or without error message

--- utils.c
+++ utils.c
@@ -9,3 +9,32 @@ int positive_modulo(int n, int mod){
 int int_center(int x, int y){
     return (x-y)/2;
 }
+
+int pointer_is_not_null(void* p, int log_error){
+    ///TO_TEST
+    if(p==NULL){
+        if(log_error)
+            fprintf(stderr,"ERROR: input pointer is null\n");
+        return 0;
+    }
+    return 1;
+}
+
+int pointer_is_null(void* p, int log_error){
+    ///TO_TEST
+    return !pointer_is_not_null(p,log_error);
+}
+
+int pointer_has_not_null_value(void** p, int log_error){
+    ///TO_TEST
+    if(p==NULL || *p==NULL){
+        if(log_error)
+            fprintf(stderr,"ERROR: input pointer is null or has null value\n");
+        return 0;
+    }
+    return 1;
+}
+
+int pointer_has_null_value(void** p, int log_error){
+    return !pointer_has_not_null_value(p,log_error);
+}
--- utils.h
+++ utils.h
@@ -1,6 +1,15 @@
 #ifndef DEF_CARC_UTILS
 #define DEF_CARC_UTILS
+#define FUNC_SUCCESS 0
+#define FUNC_FAIL -1
+#include <stdlib.h>
+#include <stdio.h>
 
 int positive_modulo(int, int);
 int int_center(int, int);
+int pointer_is_not_null(void*, int);
+int pointer_has_not_null_value(void**, int);
+int pointer_is_null(void*, int);
+int pointer_has_null_value(void**, int);
+
 #endif // DEF_CARC_UTILS
